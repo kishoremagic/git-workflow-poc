c16344c28062b22574cc1bc4d8f44a4e5b0d946a
Kishore Poc <kishore.jabu@gmail.com>
2023-03-31 16:22:49 +0530

    Updated code

--- src/test/java/StepDefinitions/BaseClass.java
+++ src/test/java/StepDefinitions/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class BaseClass {
